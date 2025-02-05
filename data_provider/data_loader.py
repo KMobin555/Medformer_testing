0fd82d1d2ce729c4f1c22be51ebd7fe20e0af8bd
KMobin555 <kamrujjamanmobin123@gmail.com>
2025-02-05 13:56:55 +0600

    MIMIC-IV data loader 4

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -815,7 +815,7 @@ class MIMICIVLoader(Dataset):
         print("subject_label ", subject_label)
         for filename in os.listdir(data_path):
             filenames.append(filename)
-        filenames = natsorted(filenames)
+        # filenames = natsorted(filenames)
         if flag == "TRAIN":
             ids = self.train_ids
             # print("train ids:", ids)
@@ -834,10 +834,11 @@ class MIMICIVLoader(Dataset):
             print("all ids len:", len(ids))
 
         for j in filenames:
+            print("ids ",ids)
             print(j)
             target_value = j.split("_")[1].split(".")[0]
             print(target_value)
-            trial_label = np.where(subject_label[:, 1] == target_value)[0]
+            trial_label = np.where(subject_label[:, 1] == target_value)[0][0]
             path = data_path + j
             print("path ", path)
             print("trail label ", trial_label)
